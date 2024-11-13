b8054bc2bea02b95b7c2e4ccf709e101c1fcfef1
Bat-Enkh <batenkh.b@pinecone.mn>
2024-11-13 12:59:00 +0800

    updateed

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,13 @@
 import express from "express";
+import cors from "cors";
 
 const server = express();
 const PORT = 8000;
 
+server.use(cors());
+
 server.get("/", (request, response) => {
-  response.send("Hello world");
+  response.send(["Hello world"]);
 });
 
 server.listen(PORT, () => {
